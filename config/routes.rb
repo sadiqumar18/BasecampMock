fb08ffe0115bff3a83575689b2f3f1311f3bdb17
unknown <sadiqumar18@gmail.com>
2020-10-05 15:17:40 +0100

    made changes to auth controller and implemented signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # registration and login routes
-  get :login, to: "auth#create"
+
+  #handles registration
   get :register, to: "auth#register"
+  post :register, to: "auth#create_account"
+
+  #handles login
+  get :login, to: "auth#login"
+  post :login, to: "auth#login"
+
   get :logout, to: "auth#logout"
 
   root to: "auth#register"
