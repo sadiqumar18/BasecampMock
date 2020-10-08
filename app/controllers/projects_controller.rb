8d567ff8c147b2f6b024abfd71a356e4b84e3ad8
unknown <sadiqumar18@gmail.com>
2020-10-08 11:11:59 +0100

    added user relationship

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -30,7 +30,7 @@ class ProjectsController < ApplicationController
   # POST /projects
   # POST /projects.json
   def create
-    @project = Project.new(project_params)
+    @project = current_user.projects.build(project_params)
 
     respond_to do |format|
       if @project.save
