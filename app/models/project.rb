4ea2e97ba465117b219026b6f77e524d0f7e4abb
unknown <sadiqumar18@gmail.com>
2020-10-08 09:11:49 +0100

    fix duplicate validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,20 +1,12 @@
 class Project < ApplicationRecord
+  has_many :tasks
+  belongs_to :user
+
   validates(:name, presence: true)
   validates(:description, presence: true)
   validates_uniqueness_of(:name)
 
-<<<<<<< HEAD
-  has_many :tasks
-  belongs_to :user
-=======
-    validates :name, presence: true
-    validates :description, presence: true
-
-    has_many :tasks
-    belongs_to :user
-
-    def day
-        self.created_at.strftime("%b %e, %y")
-    end
->>>>>>> 5d32f3c9a376c44f96e4b094d094a56d9acb09db
+  def day
+    self.created_at.strftime("%b %e, %y")
+  end
 end
