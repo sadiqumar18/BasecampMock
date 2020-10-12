3bca2a4a2f4187cadd3e263e9827b476bf8bac81
unknown <sadiqumar18@gmail.com>
2020-10-12 15:00:54 +0100

    fixed user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,9 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    if current_user.is_admin
+      @user = User.find(params[:id])
+    end
   end
 
   # POST /users
