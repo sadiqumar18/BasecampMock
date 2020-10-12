d6b2855d6f73eeff223afba4d632587382d7c083
sebaztyn <sebastinechima@gmail.com>
2020-10-13 01:56:33 +1200

    Working on the user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,8 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
+      user_details = user_params
+      print(user_details, ">>>>>>>>>>>><<<<<<<<<<<<<")
       if User.find(params[:id]).update(user_params)
         format.html { redirect_to @user, notice: "User was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
