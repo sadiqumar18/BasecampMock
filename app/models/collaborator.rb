2f2873ef45b3507b45112493e4ca2c9bf82dd944
sebaztyn <sebastinechima@gmail.com>
2020-10-20 10:04:16 +0100

    Fix collaborator bugs

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -1,5 +1,5 @@
 class Collaborator < ApplicationRecord
   belongs_to :user
   belongs_to :project
-  validates :user_id, uniqueness: {message: "User is already part of the project"}
+  validates :user_id, presence: {message: "Please provide a user email"}
 end
--- app/models/thread_message.rb
+++ app/models/thread_message.rb
@@ -1,2 +1,5 @@
 class ThreadMessage < ApplicationRecord
+  has_many :thread_messages
+  belongs_to :project_thread
+  validates(:message, presence: true)
 end
