ab2d53c46069261efeef7675f798344418daac7b
Momoh Nobert <okhai0112@gmail.com>
2020-10-23 14:19:37 +0100

    db changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_104403) do
+ActiveRecord::Schema.define(version: 2020_10_22_131234) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "attachments", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "project_id", null: false
+    t.string "project_id"
+    t.string "uploaded_by"
     t.text "file_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["project_id"], name: "index_attachments_on_project_id"
-    t.index ["user_id"], name: "index_attachments_on_user_id"
   end
 
   create_table "collaborators", force: :cascade do |t|
@@ -61,10 +80,7 @@ ActiveRecord::Schema.define(version: 2020_10_14_104403) do
   end
 
   create_table "thread_messages", force: :cascade do |t|
-    t.string "thread_id"
     t.text "message"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.integer "thread_message_id"
     t.integer "project_thread_id", null: false
     t.integer "user_id", null: false
@@ -85,8 +101,7 @@ ActiveRecord::Schema.define(version: 2020_10_14_104403) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "attachments", "projects"
-  add_foreign_key "attachments", "users"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "collaborators", "projects"
   add_foreign_key "collaborators", "users"
   add_foreign_key "project_threads", "projects"
